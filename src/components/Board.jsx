5d05a711aa5ede186a66d386d400ad5a69710562
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2020-05-27 22:20:25 +0530

    Add states to Board Component and make Square component a controlled component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import Square from './Square';
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  renderSquare(i) {
+    return <Square
+             value={this.state.squares[i]}
+             onClick={() => this.handleClick(i)} 
+           />;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   render () {
     const status = 'Next Player: X';
 
@@ -9,19 +29,19 @@ class Board extends Component {
       <div className='board'>
         <div className='status'>{status}</div>
         <div className='board-row'>
-          <Square />
-          <Square />
-          <Square />
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className='board-row'>
-          <Square />
-          <Square />
-          <Square />
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className='board-row'>
-          <Square />
-          <Square />
-          <Square />
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
         </div>
       </div>
     );
